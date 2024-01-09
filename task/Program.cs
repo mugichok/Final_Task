55ae56545da7ad8f434e67be2a7a8e951db6abec
gendfrodit <farengate20017@gmail.com>
2024-01-09 23:59:07 +0300

    create first method

---
+++ task/Program.cs
@@ -0,0 +1,10 @@
+﻿void array(string [] stringArray)
+{
+  for (int i = 0; i < stringArray.Length; i++)
+  {
+     Console.WriteLine($"Введите {i+1} элемент массива: ");
+     stringArray[i] = Console.ReadLine()!;
+  }
+}
+
+
