19758dd3c583bb4c4289ed90df9fe55773420a05
gendfrodit <farengate20017@gmail.com>
2024-01-10 00:03:12 +0300

    create function

--- task/Program.cs
+++ task/Program.cs
@@ -1,10 +1,31 @@
-﻿void array(string [] stringArray)
+﻿void array(string[] stringArray)
 {
   for (int i = 0; i < stringArray.Length; i++)
   {
      Console.WriteLine($"Введите {i+1} элемент массива: ");
      stringArray[i] = Console.ReadLine()!;
   }
+}
+
+string[] CheckEl(string[] stringArray)
+{
+  int n = 0;
+  for (int i = 0; i < stringArray.Length; i++)
+  {
+    if(stringArray[i].Length <=3)
+    n++;
+  }
+  string[] result = new string[n];
+  int j = 0;
+  for (int i = 0; i < stringArray.Length; i++)
+  {
+    if(stringArray[i].Length <=3)
+    {
+        result[j] = stringArray[i];
+        j++;
+    }
+  }
+  return result;
 }
 
 
